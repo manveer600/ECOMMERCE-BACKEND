55ef8bba9aab20456cc56aeda2752fdeed611f8b
MANVEERSINGH <singhmanveer645@gmail.com>
2024-10-09 14:05:40 +0530

    updated get all products controller

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -43,49 +43,146 @@ exports.createProduct = async (req, res) => {
     }
 }
 
+// exports.fetchAllProducts = async (req, res) => {
+//     let query = ProductModel.find({
+//         deleted: {
+//             $ne: true
+//         }
+//     });
+//     let totalProductQuery = ProductModel.find({});
+
+//     if (req.query.category) {
+//         query = query.find({ category: req.query.category });
+//         totalProductQuery = totalProductQuery.find({ category: req.query.category });
+//     }
+
+//     if (req.query.brand) {
+//         query = query.find({ brand: req.query.brand });
+//         totalProductQuery = totalProductQuery.find({ brand: req.query.brand });
+//     }
+
+//     if (req.query._sort && req.query._order) {
+//         query = query.sort({ [req.query._sort]: req.query._order });
+//         totalProductQuery = totalProductQuery.sort({ [req.query._sort]: req.query._order });
+//     }
+
+//     const totalDocs = await totalProductQuery.countDocuments();
+//     // if (req.query._page && req.query._limit) {
+//     const pageSize = req.query._limit;
+//     const page = req.query._page;
+//     query = query.skip(pageSize * (page - 1)).limit(pageSize);
+//     // }
+
+//     try {
+//         const docs = await query.exec();
+//         // res.set('X-TOTAL-COUNT', totalDocs);
+//         console.log('products are these', docs);
+//         return res.status(201).json({
+//             success: true,
+//             message: 'All items fetched successfully',
+//             products: docs,
+//             totalDocs: totalDocs
+//         })
+//     } catch (err) {
+//         console.log('Error getting a product', err.message);
+//         return res.status(500).json({ success: false, message: err.message });
+//     }
+// }
+
+
+
+
+// exports.fetchAllProducts = async (req, res) => {
+//     try {
+//         let query = ProductModel.find({ deleted: false });
+//         let totalProductQuery = ProductModel.find({ deleted: false });
+
+//         console.log('query', query);
+//         console.log('totalProductQuery', totalProductQuery);
+
+//         if (req.query.category) {
+//             query = query.where('category').equals(req.query.category);
+//             totalProductQuery = totalProductQuery.where('category').equals(req.query.category);
+//         }
+
+//         if (req.query.brand) {
+//             query = query.where('brand').equals(req.query.brand);
+//             totalProductQuery = totalProductQuery.where('brand').equals(req.query.brand);
+//         }
+
+//         if (req.query._sort && req.query._order) {
+//             const sortOrder = req.query._order === 'asc' ? 1 : -1;
+//             query = query.sort({ [req.query._sort]: sortOrder });
+//         }
+
+//         const totalDocs = await totalProductQuery.countDocuments();
+//         console.log('total docs', totalDocs);
+
+//         if (req.query._page && req.query._limit) {
+//             const pageSize = parseInt(req.query._limit, 10);
+//             const page = parseInt(req.query._page, 10);
+//             query = query.skip(pageSize * (page - 1)).limit(pageSize);
+//         }
+
+//         const docs = await query.exec();
+//         console.log('docs', docs);
+//         // Setting the total count in the response headers if pagination is used
+//         res.set('X-TOTAL-COUNT', totalDocs);
+
+//         return res.status(200).json({
+//             success: true,
+//             message: 'All items fetched successfully',
+//             products: docs,
+//             totalDocs: totalDocs
+//         });
+//     } catch (err) {
+//         console.log('Error getting products', err.message);
+//         return res.status(500).json({ success: false, message: err.message });
+//     }
+// };
+
+
+
 exports.fetchAllProducts = async (req, res) => {
-    let query = ProductModel.find({
-        deleted: {
-            $ne: true
-        }
-    });
-    let totalProductQuery = ProductModel.find({});
-
-    if (req.query.category) {
-        query = query.find({ category: req.query.category });
-        totalProductQuery = totalProductQuery.find({ category: req.query.category });
-    }
-
-    if (req.query.brand) {
-        query = query.find({ brand: req.query.brand });
-        totalProductQuery = totalProductQuery.find({ brand: req.query.brand });
-    }
-
-    if (req.query._sort && req.query._order) {
-        query = query.sort({ [req.query._sort]: req.query._order });
-        totalProductQuery = totalProductQuery.sort({ [req.query._sort]: req.query._order });
-    }
-
-    const totalDocs = await totalProductQuery.count();
-    if (req.query._page && req.query._limit) {
-        const pageSize = req.query._limit;
-        const page = req.query._page;
-        query = query.skip(pageSize * (page - 1)).limit(pageSize);
-    }
-
     try {
-        const docs = await query.exec();
-        // res.set('X-TOTAL-COUNT', totalDocs);
-        console.log('products are these', docs);
-        return res.status(201).json({
+        const where = { deleted: false };
+        let sortFilter = {};
+        if (req.query.category) {
+            where.category = req.query.category;
+        }
+        if (req.query.brand) {
+            where.brand = req.query.brand;
+        }
+        if (req.query._sort && req.query._order) {
+            const sortOrder = req.query._order === 'asc' ? 1 : -1;
+            sortFilter = { [req.query._sort]: sortOrder };
+        }
+
+        //BASICALLY WE ARE COUNTING THE NUMBER OF DOCUMENTS WITH THE PROVIDED CONDITIONS.
+        const totalDocs = await ProductModel.countDocuments(where);
+        console.log('total documents', totalDocs);
+
+        // Pagination
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 10;
+        const skip = (page - 1) * limit;
+
+        const products = await ProductModel.find(where).sort(sortFilter).skip(skip).limit(limit);
+        res.set('X-TOTAL-COUNT', totalDocs);
+
+        console.log(products);
+        console.log(totalDocs);
+        return res.status(200).json({
             success: true,
             message: 'All items fetched successfully',
-            products: docs,
+            data: products,
             totalDocs: totalDocs
-        })
-    } catch (err) {
-        console.log('Error getting a product', err.message);
-        return res.status(500).json({ success: false, message: err.message });
+        });
+
+    } catch (error) {
+        console.log('error getting products', error.message);
+        return res.status(500).json({ success: false, message: error.message });
+
     }
 }
 
